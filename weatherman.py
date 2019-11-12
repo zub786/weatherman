53d5f93d73c8a7770dfb4ae6df14802075b8cd2c
Zubair.Shakoor <zubair@A002-00046.arbisoft.com>
2019-11-12 12:32:22 +0500

    report generator  file name changed

--- weatherman.py
+++ weatherman.py
@@ -1,7 +1,9 @@
 import sys
 import os
+from os.path import exists
 from os import system
-import weather_data_files_processor
+import weather_data_report_generator
+from parser import Parse
 import argparse
 
 # Parssing arguments
@@ -13,8 +15,10 @@ parser.add_argument("-e", "--e", help="""Flag to generate
 yearly report""", type=str)
 parser.add_argument("-a", "--a", help="""Flag to generate
 monthly report""", type=str)
-parser.add_argument("-c", "--c", help="", type=str)
-parser.add_argument("-d", "--d", help="", type=str)
+parser.add_argument("-c", "--c", help="""Flag to generate
+horizontal bar chat for highest and lowest temprature individually""", type=str)
+parser.add_argument("-d", "--d", help="""Flag to generate
+horizontal bar chat for highest and lowest temprature in same line""", type=str)
 args = parser.parse_args()
 
 if args.e:
@@ -30,10 +34,13 @@ elif args.d:
     date = args.d
     info_type = '-d'
 
-data_files_list = [x for x in os.listdir(args.path)]
-# Passing files to parse engine to get list of weather
-parsed_list = weather_data_files_processor.parse_files(date,
- data_files_list, args.path, info_type)
-# Passing files to parse engine to get list of weather
-# by passing filter data list and information type
-weather_data_files_processor.generate_report(parsed_list, info_type)
+if exists(args.path):
+    data_files_list = [x for x in os.listdir(args.path)]
+    # Passing files to parse engine to get list of weather
+    parsed_list = Parse(date, data_files_list, args.path, info_type).data
+    # Passing files to parse engine to get list of weather
+    # by passing filter data list and information type
+    weather_data_report_generator.generate_report(parsed_list, info_type)
+else:
+    print("ERROR: 404 NOT FOUND - Provided path is not found..")
+    exit()
