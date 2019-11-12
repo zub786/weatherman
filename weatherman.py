2742368e1d1ea7d48defc22a6a78dea6e56378f9
Zubair.Shakoor <zubair@A002-00046.arbisoft.com>
2019-11-12 17:10:53 +0500

    date formats management added

--- weatherman.py
+++ weatherman.py
@@ -5,6 +5,7 @@ from os import system
 import weather_data_report_generator
 from parser import Parse
 import argparse
+import re
 
 # Parssing arguments
 parser = argparse.ArgumentParser(description="""Generate weather
@@ -20,6 +21,20 @@ horizontal bar chat for highest and lowest temprature individually""", type=str)
 parser.add_argument("-d", "--d", help="""Flag to generate
 horizontal bar chat for highest and lowest temprature in same line""", type=str)
 args = parser.parse_args()
+# Regex for year argument
+regex_year_pattern=re.compile("([0-9]{4}$)")
+# Regex for month argument
+regex_month_pattern=re.compile("([0-9]{4}[/](1[0-2]|[1-9])$)")
+
+# checking arguments format
+if args.e and not regex_year_pattern.match(args.e):
+    print("Provided year is not in correct format it should be like 2000")
+    exit()
+if (((args.a) and not regex_month_pattern.match(args.a)) or
+    ((args.c) and not regex_month_pattern.match(args.c)) or
+    ((args.d) and not regex_month_pattern.match(args.d))):
+    print("Provided month is not in correct format it should be like 2000/12")
+    exit()
 
 if args.e:
     date = args.e
